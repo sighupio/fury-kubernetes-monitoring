91214ecb1630c2fc6605ccc008b7c82cbd5371e9
gitirabassi <giacomo@tirabassi.eu>
2018-12-14 17:57:49 +0100

    testing better with copper

---
+++ copper.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -x
+set -e
+set -u
+set -o pipefail
+
+for dir in $(find . -type d -maxdepth 1 -mindepth 1 -not -path "./.git" -not -path "./prometheus-operator" -not -path "./examples");
+do
+    pushd $dir
+    echo "------------- RUNNING TESTS INTO $dir ---------"
+    kustomize build > /dev/null
+    TMPFILE=$(mktemp)
+    kustomize build > $TMPFILE
+    copper check --rules ../rules.cop --file $TMPFILE
+    rm $TMPFILE
+    popd
+done
