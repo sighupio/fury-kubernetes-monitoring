f8a67d0ce3590471883e00fa69597b9fec0620a7
Giuseppe Iannelli <94362884+g-iannelli@users.noreply.github.com>
2025-02-24 16:06:32 +0100

    fix(prometheus-adapter): get the right helm chart version based on kube-prometheus version

--- utils/pull-upstream.sh
+++ utils/pull-upstream.sh
@@ -154,16 +154,20 @@ case "${FURY_MODULE}" in
   "prometheus-adapter")
     populate_package "prometheusAdapter"
     helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+    PROMETHEUS_ADAPTER_RELEASE=$(cat apiService.yaml | yq '.metadata.labels."app.kubernetes.io/version"')
+    cat apiService.yaml | yq '.metadata.labels' > "${WORK_DIR}/labels.yml"
+    KUBE_PROMETHEUS_ADAPTER_RELEASE=$(helm search repo prometheus-adapter -l -o json | jq -r --arg PROMETHEUS_ADAPTER_RELEASE "${PROMETHEUS_ADAPTER_RELEASE}" '.[] | select(.app_version == ("v"+$PROMETHEUS_ADAPTER_RELEASE)) | .version')
     cd "${WORK_DIR}"
     helm template \
       --release-name prometheus-adapter \
       --namespace monitoring \
       -f "${VALUES_PATH}/prometheus-adapter.yml" \
       --api-versions apiregistration.k8s.io/v1 \
+      --version "${KUBE_PROMETHEUS_ADAPTER_RELEASE}" \
       prometheus-community/prometheus-adapter | yq --split-exp '.kind + "-" + .metadata.name'
     cd -
-    cat "${WORK_DIR}/APIService-v1beta1.custom.metrics.k8s.io" >> "${KATALOG_PATH}/prometheus-adapter/apiService.yaml"
-    cat "${WORK_DIR}/APIService-v1beta1.external.metrics.k8s.io" >> "${KATALOG_PATH}/prometheus-adapter/apiService.yaml"
+    cat "${WORK_DIR}/APIService-v1beta1.custom.metrics.k8s.io" | grep -v '#' | yq "del(.metadata.labels) | .metadata.labels = (load(\"${WORK_DIR}/labels.yml\"))" >> "${KATALOG_PATH}/prometheus-adapter/apiService.yaml"
+    cat "${WORK_DIR}/APIService-v1beta1.external.metrics.k8s.io" | grep -v '#' | yq "del(.metadata.labels) | .metadata.labels = (load(\"${WORK_DIR}/labels.yml\"))" >> "${KATALOG_PATH}/prometheus-adapter/apiService.yaml"
     yq -i ".data = (load(\"${WORK_DIR}/ConfigMap-prometheus-adapter.yml\") | .data)" configMap.yaml
     yq -i '.rules[0].apiGroups = ["metrics.k8s.io", "custom.metrics.k8s.io", "external.metrics.k8s.io"]' clusterRoleServerResources.yaml
     ;;
